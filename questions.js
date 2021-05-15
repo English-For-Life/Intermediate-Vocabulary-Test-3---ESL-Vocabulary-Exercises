52010a452e19ff088153297fad489b7c84652f28
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-15 18:11:38 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"The poet had written several ............... before completing the final version.",
+        options:["drafts", "originals", "poems", "novels", "rehearsals"],
+        answer: 0
+    },
+    {
+        q:"E-mails are normally ............... seconds after they have been sent.",
+        options:["copied", "delivered", "erased", "transformed", "inspected"],
+        answer: 1
+    },
+    {
+        q:"He fell on his knees, ............... for mercy.",
+        options:["respecting", "protesting", "begging", "resisting", "demanding"],
+        answer: 2
+    },
+    {
+        q:"Every working person must pay income ...............   .",
+        options:["tax", "salary", "wage", "price", "payment"],
+        answer: 0
+    },
+    {
+        q:"It is quite ............... that they had already made up their minds about it.",
+        options:["chance", "opportunity", "possibility", "probability", "probable"],
+        answer: 4
+    },
+    {
+        q:"The patient was ............... examined by three fully-qualified doctors.",
+        options:["thoroughly", "fondly", "suggestively", "softly", "shyly"],
+        answer: 0
+    },
+    {
+     q:"Buses and trolleybuses ............... the suburbs to the city center.",
+        options:["conduct", "connect", "refer", "concern", "conclude"],
+        answer: 1   
+    },
+    {
+        q:"The house was completely ............... by tall trees.",
+        options:["suspended", "surrendered", "surrounded", "sacrificed", "supported"],
+        answer: 2
+    },
+    {
+        q:"A hand grenade ............... just a few meters from where he was lying.",
+        options:["achieved", "knocked", "exposed", "exploded", "zoomed"],
+        answer: 3
+    },
+    {
+        q:"A piece of shrapnel sliced his right cheek, as a hand grenade ............... him down.",
+        options:["concluded", "exposed", "zoomed", "exploded", "knocked"],
+        answer: 4
+    }
+]
